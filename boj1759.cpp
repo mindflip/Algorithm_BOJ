197e6368aa7f8da547af64731c34809a5e1a253a
mindflip <hmu1212@hotmail.com>
2021-01-03 19:44:00 +0900

    delete unused variables boj1759

--- boj1759.cpp
+++ boj1759.cpp
@@ -7,8 +7,6 @@ using namespace std;
 int l, c;
 vector<char> v, temp;
 vector<string> res;
-bool visit[16];
-char vowels[5] = {'a', 'e', 'i', 'o', 'u'};
 
 void findAllPassword(int cnt, int vCnt, int cCnt) {
     if(temp.size() == l) {
@@ -16,20 +14,15 @@ void findAllPassword(int cnt, int vCnt, int cCnt) {
         for(char c : temp) {
             s += c;
         }
-        // cout << s << endl;
         if(vCnt >= 1 && cCnt >= 2) res.push_back(s);
         return;
     }
     
     for(int i = cnt; i < v.size(); ++i) {
-        if(!visit[i]) {
-            visit[i] = true;
-            temp.push_back(v[i]);
-            if(v[i] == 'a' || v[i] == 'e' || v[i] == 'i' || v[i] == 'o' || v[i] == 'u') findAllPassword(i + 1, vCnt + 1, cCnt);
-            else findAllPassword(i + 1, vCnt, cCnt + 1);
-            temp.pop_back();
-            visit[i] = false;
-        }
+        temp.push_back(v[i]);
+        if(v[i] == 'a' || v[i] == 'e' || v[i] == 'i' || v[i] == 'o' || v[i] == 'u') findAllPassword(i + 1, vCnt + 1, cCnt);
+        else findAllPassword(i + 1, vCnt, cCnt + 1);
+        temp.pop_back();
     }
 }
 
@@ -45,7 +38,6 @@ int main() {
     sort(v.begin(), v.end());
 
     findAllPassword(0, 0, 0);
-    
     
     for(string s : res) {
         cout << s << endl;
